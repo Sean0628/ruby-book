c5185b7850d32839590ad35f214f229eed106f63
Sho Ito <i.sho.0628@gmail.com>
2018-04-28 14:34:58 +0900

    finish fizz_buzz

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,1 +1,17 @@
-puts 'Hello, world!'
+def fizz_buzz
+  num = 1
+  while num <= 100
+    if num % 15 == 0
+      puts 'FizzBuzz'
+    elsif num % 3 == 0
+      puts 'Fizz'
+    elsif num % 5 == 0
+      puts 'Buzz'
+    else
+      puts num
+    end
+    num += 1
+  end
+end
+
+fizz_buzz
