71f7d3e4559fa0911e6ccf65fd66b518f0140e21
Sho Ito <i.sho.0628@gmail.com>
2018-04-29 15:43:13 +0900

    modify fizzbuzz, and add test to it

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,17 +1,17 @@
-def fizz_buzz
-  num = 1
-  while num <= 100
-    if num % 15 == 0
-      puts 'FizzBuzz'
-    elsif num % 3 == 0
-      puts 'Fizz'
-    elsif num % 5 == 0
-      puts 'Buzz'
-    else
-      puts num
-    end
-    num += 1
+def fizz_buzz(num)
+  if num % 15 == 0
+    'FizzBuzz'
+  elsif num % 3 == 0
+    'Fizz'
+  elsif num % 5 == 0
+    'Buzz'
+  else
+    num.to_s
   end
 end
 
-fizz_buzz
+num = 1
+while num <= 100
+  puts fizz_buzz(num)
+  num += 1
+end
